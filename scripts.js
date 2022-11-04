7962fd8954590302e7725ee54d281a82133ad897
Olajide <clickcom007@yahoo.com>
2022-11-04 14:45:05 +0100

    add readme

--- scripts.js
+++
@@ -1,7 +0,0 @@
-const burger = document.querySelector(".burger");
-const nav = document.querySelector("nav");
-
-burger.addEventListener("click", () => {
-  nav.classList.toggle("open");
-  burger.classList.toggle("open");
-});
