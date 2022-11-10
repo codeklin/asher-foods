de6016ff39bd3a427b23f782fc83def90712dc37
Olajide <clickcom007@yahoo.com>
2022-11-10 01:26:55 +0100

    style hamburger

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,1 +1,7 @@
-//JS
+const menuToggler = document.querySelector('.menu-toggler');
+const navbar = document.querySelector('.navbar');
+const collapsibleMenu = document.querySelector('.collapsible-menu');
+
+menuToggler.addEventListener('click', () => {
+    navbar.classList.toggle('show-menu');
+});
